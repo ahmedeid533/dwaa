e2d5f2f3f37abb1824cd3a8f00fcfaf4be101523
ahmedeid533 <ahmedeidgoma333@gmail.com>
2025-07-16 19:27:01 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,22 +16,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const navigate = useRouter();
-  const pathname = usePathname();
-	const isAuth = JSON.parse(localStorage.getItem('isAuthenticated') || 'false');
-	useEffect(() => {
-		console.log("pathname ==> ", pathname);
-	console.log("isAuth ==> ", isAuth);
-		if (pathname == '/' || !isAuth) {
-			navigate.push('/dashboard/signin');
-			console.log("redirecting to signin page");
-		} else if (!pathname.includes('/dashboard') && isAuth) {
-			let copypath = pathname
-			copypath = copypath.at(0) == '/' ? copypath.slice(1) : copypath;
-			copypath = '/dashboard/'.concat(copypath);
-			navigate.push(copypath);
-		}
-	}, [pathname, navigate]);
+
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
